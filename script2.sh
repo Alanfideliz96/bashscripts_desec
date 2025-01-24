129d682b50110f149782e5ad6d82d8b9149dddcd
Alan <alanfideliz96@gmail.com>
2025-01-24 09:55:42 -0300

    Alteração do script2.sh, adicionei tratativas para o input do usuário

--- script2.sh
+++ script2.sh
@@ -2,14 +2,30 @@
 echo "Qual a cor do semaforo?"
 read cor
 
-if [ "$cor" == "verde" ]
+if [[ "$cor" =~ ^[0-9]+$ ]];
 then
-	echo "Siga em frente :)"
+        echo "Cara só aceito cores"
+        echo "verde, amarelo ou vermelho"
+
+elif [ "$cor" == "" ]
+then
+        echo "Sinaleiro em Bash"
+        echo "Me fale uma cor do semaforo"
+        echo "verde, amarelo ou vermelho"
+
+elif [ "$cor" == "verde" ]
+then
+        echo "Siga em frente :)"
 
 elif [ "$cor" == "amarelo" ]
 then
-	echo "Aguarde"
+        echo "Aguarde"
 
+elif [ "$cor" == "vermelho" ]
+then
+        echo "PARE !!!"
 else
-	echo "PARE !!!"
+        echo "Só aceito as seguintes palavras:"
+        echo "verde, amarelo, vermelho"
+        echo -e "\n\nOBS: Tente escrever do jeito descrito acima \n pois o programador não tratou \nas >
 fi
